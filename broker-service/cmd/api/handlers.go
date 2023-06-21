5278d1acd7f67f37196b4332cb93428a014e441d
Prasanna Adiga <prasanna@Prasannas-MacBook-Air.local>
2023-06-21 19:50:43 +0530

    Added docker file and docker-compose file

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -16,7 +16,6 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Error:   false,
 		Message: "Hit the broker",
 	}
-
 	out, _ := json.MarshalIndent(payload, "", "\t")
 	w.Header().Set("Content-Type", "application-json")
 	w.WriteHeader(http.StatusAccepted)
